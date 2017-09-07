edbffe8a66f6c006d3e152855fa7c420aa1069a3
Costin Ionescu <costin.ionescu@gmail.com>
2017-09-06 20:00:53 -0700

    fix building and linking against libs under mingw

--- bld/mingw32.mk
+++ bld/mingw32.mk
@@ -1,8 +1,22 @@
 builder := mingw32
 bld_mode := cc
 
-p := x86_64-w64-mingw32-
+p := i686-w64-mingw32-
 cc_name := gcc
 cxx_name := g++
+
+exe_suffix := .exe
+dlib_prefix :=
+dlib_suffix := .dll
+
+define dlib_export
+	mkdir -p $(dir $1)../bin
+	cp -f $2 $(dir $1)../bin/$(notdir $1)
+	cp -f $2.a $1.a
+endef
+
+CFLAGS :=
+CXXFLAGS :=
+LDFLAGS = -Wl,--out-implib,$@.a
 
 include $b/cc-builder.mk
--- bld/mingw64.mk
+++ bld/mingw64.mk
@@ -5,13 +5,18 @@ p := x86_64-w64-mingw32-
 cc_name := gcc
 cxx_name := g++
 
-dlib_out_dir := bin
 exe_suffix := .exe
 dlib_prefix :=
 dlib_suffix := .dll
 
+define dlib_export
+	mkdir -p $(dir $1)../bin
+	cp -f $2 $(dir $1)../bin/$(notdir $1)
+	cp -f $2.a $1.a
+endef
+
 CFLAGS :=
 CXXFLAGS :=
+LDFLAGS = -Wl,--out-implib,$@.a
 
 include $b/cc-builder.mk
-
